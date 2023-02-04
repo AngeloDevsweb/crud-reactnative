45f0bcef6de03937cbb5ea4b18eaea813828c260
Angelo <gabriel_propio95@hotmail.com>
2023-02-04 17:06:48 -0400

    proyecto completo

---
+++ credenciales.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLBzfUmOHZIgfdRf-9rwIypDXfSbmOMlA",
+  authDomain: "appreactnative-278ee.firebaseapp.com",
+  projectId: "appreactnative-278ee",
+  storageBucket: "appreactnative-278ee.appspot.com",
+  messagingSenderId: "725243953197",
+  appId: "1:725243953197:web:996b49537e0e21f57238a4"
+};
+
+// Initialize Firebase
+const FirebaseApp = initializeApp(firebaseConfig);
+
+export default FirebaseApp;
